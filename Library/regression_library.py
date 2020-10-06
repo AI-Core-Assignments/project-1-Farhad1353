0d62d55fe1ca76b20c6c75cdf5494be259187e76
Farhad Saiepour <fsaie74@yahoo.co.uk>
2020-10-06 14:40:10 +0100

    further amendments

--- Library/regression_library.py
+++ Library/regression_library.py
@@ -14,10 +14,67 @@ def plot_models(X, Y, title_name, colour = 'r'):
     plt.ylabel('Accuracy %')
     plt.show()
 
+def hyper_2_param_array(param1_start, param1_end, param1_step, param2_start, param2_end, param2_step):
+    '''
+    hyper_2_param_array function creates a 2 dimension array 
+    which the rows are representing each model to be trained and 
+    the columns are representing each of the 2 HyperParameters 
+    chosen for the training
+    '''
+
+    param1_total = int((param1_end + param1_step - param1_start-1)/param1_step)
+    param2_total = int((param2_end + param2_step - param2_start-1)/param2_step)
+
+   
+    total_models = param1_total * param2_total
+    param_array = np.ones((total_models, 2), dtype=int)
+    param_array_idx = 0
+    for param1_value in range(param1_start, param1_end, param1_step):
+        for param2_value in range(param2_start, param2_end, param2_step):
+            param_array[param_array_idx, 0] = int(param1_value)
+            param_array[param_array_idx, 1] = int(param2_value)
+            param_array_idx+=1         
+    return param_array
+
+def hyper_3_param_array(param1_start, param1_end, param1_step, \
+                      param2_start, param2_end, param2_step, \
+                      param3_start, param3_end, param3_step):
+    '''
+    hyper_3_param_array function creates a 2 dimension array 
+    which the rows are representing each model to be trained and 
+    the columns are representing each of the 3 HyperParameters 
+    chosen for the training
+    '''
+
+    param1_total = int((param1_end + param1_step - param1_start-1)/param1_step)
+    param2_total = int((param2_end + param2_step - param2_start-1)/param2_step)
+    param3_total = int((param3_end + param3_step - param3_start-1)/param3_step)
+   
+    total_models = param1_total * param2_total * param3_total
+    param_array = np.ones((total_models, 3), dtype=int)
+    param_array_idx = 0
+    for param1_value in range(param1_start, param1_end, param1_step):
+        for param2_value in range(param2_start, param2_end, param2_step):
+            for param3_value in range(param3_start, param3_end, param3_step):
+                param_array[param_array_idx, 0] = int(param1_value)
+                param_array[param_array_idx, 1] = int(param2_value)
+                param_array[param_array_idx, 2] = int(param3_value)
+                param_array_idx+=1         
+    return param_array
+
+
 
 def low_high_param(mid, step, param=3):
+    '''
+    low_high_param is a function which sets the lowest and
+    highest figure that we want to tune our HyperParameters
+    by getting the midpoint(which was the best HyperParameters)
+    from previous stage.
+    '''
     if param == 3:
         low = mid - step
+        if mid <= step:
+            low = mid
         high = mid + step + 1
     else:
         low = mid - (step * 2)
